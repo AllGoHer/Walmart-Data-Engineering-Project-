53e5893693ef044557d5b17d6ab76098273a1552
Allan Gonzales <164255418+AllGoHer@users.noreply.github.com>
2026-09-09 17:03:37 -0500

    Add files via upload

---
+++ walmart_project/models/gold/eph_stores.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='view') }}
+
+SELECT
+    DISTINCT 
+    store_id,
+    store_name,
+    store_city,
+    store_province,
+    store_country,
+    store_created_timestamp,
+    store_updated_timestamp,
+    store_is_active,
+    store_processed_at,
+    CURRENT_TIMESTAMP() AS stores_gold_processed_at
+FROM
+    {{ ref('obt_b') }}
